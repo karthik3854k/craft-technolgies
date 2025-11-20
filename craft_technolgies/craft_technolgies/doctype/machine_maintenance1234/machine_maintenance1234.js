0fc32dae04d3de9f1be6ccdb9658292eabab3032
Harish <harish@Harishs-MacBook-Air.local>
2025-11-20 20:22:11 +0530

    Update: removed some unwanted changes

---
+++ craft_technolgies/craft_technolgies/doctype/machine_maintenance1234/machine_maintenance1234.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, karthik and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Machine Maintenance1234", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ craft_technolgies/craft_technolgies/doctype/machine_maintenance1234/test_machine_maintenance1234.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, karthik and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMachineMaintenance1234(FrappeTestCase):
+	pass
